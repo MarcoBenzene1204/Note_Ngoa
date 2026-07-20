fd1daa893a2f84b4d5bb0e3a39758a41ad241d7d
MarcoBenzene1204 <kemognemarcobenzene@gmail.com>
2026-07-20 10:40:43 +0100

    Commit: Initialisation du projet

---
+++ src/main/java/com/marco_aldiss/Le_Soko/LeSokoApplication.java
@@ -0,0 +1,13 @@
+package com.marco_aldiss.Le_Soko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeSokoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeSokoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marco_aldiss/Le_Soko/LeSokoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marco_aldiss.Le_Soko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeSokoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

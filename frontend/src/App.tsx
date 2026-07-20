e57eb4a5bf5b36d7b34d321c23d5b528900800ab
MarcoBenzene1204 <kemognemarcobenzene@gmail.com>
2026-07-20 15:30:47 +0100

    commit: Ajout des composants visuels de shadcn

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { LoginForm } from "@/components/login-form"
+import './App.css'
+import { SignupForm } from "@/components/signup-form"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div className="w-full min-h-screen flex items-center justify-center"></div>
+  )
+}
+
+export default App
